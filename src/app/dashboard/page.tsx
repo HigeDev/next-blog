509fccb9e34a8839ec3d38b6fcc4b491f9e33f68
HigeDev <dominicusagfid@gmail.com>
2025-04-15 12:09:47 +0700

    fix dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,27 +1,10 @@
-"use client";
+import { Suspense } from "react";
+import DashboardClient from "./DashboardClient";
 
-import { useEffect, useState } from "react";
-import DashSidebar from "../components/DashSidebar";
-import DashProfile from "../components/DashProfile";
-import { useSearchParams } from "next/navigation";
-export default function Dashboard() {
-  const searchParams = useSearchParams();
-  const [tab, setTab] = useState("");
-  useEffect(() => {
-    const urlParams = new URLSearchParams(searchParams);
-    const tabFromUrl = urlParams.get("tab");
-    if (tabFromUrl) {
-      setTab(tabFromUrl);
-    }
-  }, [searchParams]);
+export default function Page() {
   return (
-    <div className="min-h-screen flex flex-col md:flex-row">
-      <div className="md:w-56">
-        {/* Sidebar */}
-        <DashSidebar />
-      </div>
-      {/* profile... */}
-      {tab === "profile" && <DashProfile />}
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <DashboardClient />
+    </Suspense>
   );
 }
