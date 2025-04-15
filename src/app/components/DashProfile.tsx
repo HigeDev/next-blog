221da35d0f6932fa68fb4fa60201ba0de8f072e7
HigeDev <dominicusagfid@gmail.com>
2025-04-15 10:54:43 +0700

    fix dashboard

--- src/app/components/DashProfile.tsx
+++ src/app/components/DashProfile.tsx
@@ -4,7 +4,6 @@ import { UserProfile } from "@clerk/nextjs";
 import { useTheme } from "next-themes";
 
 export default function DashProfile() {
-  const { theme } = useTheme();
   return (
     <div className="flex justify-center items-center w-full">
       <UserProfile routing="hash" />
