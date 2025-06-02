d50f0f73f9f84d8560261f675872b3e00f5eb196
HigeDev <dominicusagfid@gmail.com>
2025-06-02 20:29:08 +0700

    add seo

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import AboutClient from "../components/AboutClient";
+import Head from "next/head";
 
 export const metadata = {
   title: "About",
   description:
-    "Welcome to my blog, Discover a variety of articles and tutorials reflect on my personal journey",
+    "Learn more about Dominicus Agfid and the journey behind HigeSan.",
   openGraph: {
     title: "About | HigeSan",
     description:
-      "Welcome to my blog, Discover a variety of articles and tutorials reflect on my personal journey",
+      "Learn more about Dominicus Agfid and the journey behind HigeSan.",
     url: "https://higesan.store",
     siteName: "HigeSan",
     images: [
@@ -26,10 +27,39 @@ export const metadata = {
     card: "summary_large_image",
     title: "About | HigeSan",
     description:
-      "Welcome to my blog, Discover a variety of articles and tutorials reflect on my personal journey",
+      "Learn more about Dominicus Agfid and the journey behind HigeSan.",
     images: ["https://higesan.store/Hige-Logo.png"],
   },
 };
 export default function About() {
-  return <AboutClient />;
+  return (
+    <>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "BreadcrumbList",
+              itemListElement: [
+                {
+                  "@type": "ListItem",
+                  position: 1,
+                  name: "Home",
+                  item: "https://higesan.store",
+                },
+                {
+                  "@type": "ListItem",
+                  position: 2,
+                  name: "About",
+                  item: "https://higesan.store/about",
+                },
+              ],
+            }),
+          }}
+        />
+      </Head>
+      <AboutClient />
+    </>
+  );
 }
--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ProjectClient from "../components/ProjectClient";
+import Head from "next/head";
 
 export const metadata = {
   title: "Project",
-  description:
-    "Welcome to my blog, Discover a variety of articles and tutorials reflect on my personal journey",
+  description: "Explore various projects and works developed under HigeSan",
   openGraph: {
     title: "Project | HigeSan",
-    description:
-      "Welcome to my blog, Discover a variety of articles and tutorials reflect on my personal journey",
+    description: "Explore various projects and works developed under HigeSan",
     url: "https://higesan.store",
     siteName: "HigeSan",
     images: [
@@ -25,11 +24,39 @@ export const metadata = {
   twitter: {
     card: "summary_large_image",
     title: "Project | HigeSan",
-    description:
-      "Welcome to my blog, Discover a variety of articles and tutorials reflect on my personal journey",
+    description: "Explore various projects and works developed under HigeSan",
     images: ["https://higesan.store/Hige-Logo.png"],
   },
 };
 export default function Project() {
-  return <ProjectClient />;
+  return (
+    <>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "BreadcrumbList",
+              itemListElement: [
+                {
+                  "@type": "ListItem",
+                  position: 1,
+                  name: "Home",
+                  item: "https://higesan.store",
+                },
+                {
+                  "@type": "ListItem",
+                  position: 2,
+                  name: "Project",
+                  item: "https://higesan.store/projek",
+                },
+              ],
+            }),
+          }}
+        />
+      </Head>
+      <ProjectClient />
+    </>
+  );
 }
